0d471ee7e9a1e782a1ffc2b29cae61fad22d7db3
Evan Farr <roboproductions200@gmail.com>
2020-04-13 00:35:39 -0600

    finish sorting bubblesort, fix insertionsort

--- Sort.java
+++ Sort.java
@@ -17,27 +17,53 @@ public class Sort {
         System.out.println(list);
     }
 
+    static void bubbleSort(ArrayList <String> list) {
+        boolean isSorted = false;
+
+        while (!isSorted) {
+            int sorted = 0;
+            for (int i = 1; i < list.size(); i++) {
+                String current = list.get(i);
+                int j = i -1;
+                String swap = list.get(j);
+                if (current.compareTo(swap) < 0) {
+                    list.set(i, swap);
+                    list.set(j, current);
+                }
+                if (current.compareTo(swap) >= 0) {
+                    sorted++;
+                }
+                if (sorted == list.size() - 1) {
+                    isSorted = true;
+                }
+
+            }
+        }
+    }
+
     static void insertionSort(ArrayList <String> list){
-            for(int i = 1; i < list.size(); i++){
+            for (int i = 1; i < list.size(); i++) {
                 String current= list.get(i);
                 int j = i - 1;
                 String swap = list.get(j); 
-                if (current.compareTo(swap) < 0){
+                while (j >= 0) {
                     swap = list.get(j);
-                    list.set(i, swap);
-                    list.set(j, current);
-                    j -= 1; 
+                    if (current.compareTo(swap) < 0) {
+                        list.set(j + 1, swap);
+                        list.set(j, current);
+                    }
+                    j -= 1;
                 }
             }
     }
 
     static void selectionSort(ArrayList<String> list){
-        for(int i = 0 ; i < list.size(); i++){
+        for (int i = 0 ; i < list.size(); i++) {
             String first = list.get(i);
             int firstLocation = i;
             String current= list.get(i); 
-            for(int j = i + 1; j < list.size(); j++){
-                if(first.compareTo(list.get(j)) >= 0){
+            for (int j = i + 1; j < list.size(); j++) {
+                if (first.compareTo(list.get(j)) >= 0) {
                     first = list.get(j);
                     firstLocation = j;
                 }
